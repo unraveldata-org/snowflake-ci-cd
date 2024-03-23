540404414f2e033cecef0d1c9d01005d7697ec67
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-23 15:46:43 +0530

    Create snowflake_cicd_demo.sql

---
+++ Unravel_Snowflake/snowflake_cicd_demo.sql
@@ -0,0 +1,10 @@
+SELECT * FROM employee6 CROSS JOIN orders2 order by employee6.id;
+
+
+SELECT * 
+ FROM employee6912 
+ CROSS JOIN orders2721 
+ order by employee6912.id;
+
+
+SELECT * FROM table1 INNER JOIN table4 order by table1.id limit 10;
