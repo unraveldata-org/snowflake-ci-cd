36d651e943bbd51c997bfdbd6297dd87897d06ce
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-26 14:49:13 +0530

    Update snowflake_cicd_demo.sql

--- Unravel_Snowflake/snowflake_cicd_demo.sql
+++ Unravel_Snowflake/snowflake_cicd_demo.sql
@@ -3,7 +3,7 @@ SELECT
         o.O_ORDERSTATUS as "Order Status"
     FROM
         ORDERS as o
-    CROSS JOIN CUSTOMER as c ORDER BY c.c_name;
+    INNER JOIN CUSTOMER as c ORDER BY c.c_name LIMIT 10;
 
 select WR_ORDER_NUMBER,WR_ITEM_SK from sfsalesshared_sfc_samples_va3_sample_data.tpcds_sf100tcl.web_returns where wr_fee >= 0.22 order by WR_ORDER_NUMBER,WR_ITEM_SK ;
 
