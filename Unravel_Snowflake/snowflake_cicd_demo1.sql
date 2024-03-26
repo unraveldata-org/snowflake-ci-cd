c5dc102204703f15b1f6a1eabe923c6d816c4dd6
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-26 14:27:00 +0530

    Create snowflake_cicd_demo1.sql

---
+++ Unravel_Snowflake/snowflake_cicd_demo1.sql
@@ -0,0 +1,12 @@
+SELECT
+        c.c_name as "Customer Name",
+        o.O_ORDERSTATUS as "Order Status"
+    FROM
+        ORDERS as o
+    CROSS JOIN CUSTOMER as c;
+
+select WR_ORDER_NUMBER,WR_ITEM_SK from sfsalesshared_sfc_samples_va3_sample_data.tpcds_sf100tcl.web_returns where wr_fee >= 0.22 order by WR_ORDER_NUMBER,WR_ITEM_SK ;
+
+select WR_ORDER_NUMBER,WR_ITEM_SK from sfsalesshared_sfc_samples_va3_sample_data.tpcds_sf100tcl.web_returns where wr_fee >= 0.22 limit 100;
+
+select * from sfsalesshared_sfc_samples_va3_sample_data.tpcds_sf100tcl.web_returns where wr_fee >= 0.22;
