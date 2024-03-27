d479fd12972c52a701d8bd4b14d9f3fb42885857
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-28 01:34:20 +0530

    Update github_cicd_python.py

--- github_cicd_python.py
+++ github_cicd_python.py
@@ -266,15 +266,15 @@ def format_comment(query, insights, query_line_map, details_map, url):
     # Add details map as a table
     for key, value in details_map.items():
         if key in ['minCost', 'maxCost']:
-            value_html = f'<span style="font-weight: bold;">{value}</span>'
+            value_html = f'<span style="font-weight: bold;">{value[0]}</span>'
         if key == 'byteScanned':
-            value_html = bytes_to_human_readable(value)
-        if value.lower() == "success":
-            value_html = f'<span style="color:green">{value}</span>'
-        elif value.lower() == "fail":
-            value_html = f'<span style="color:red">{value}</span>'
+            value_html = bytes_to_human_readable(value[0])
+        if value[0].lower() == "success":
+            value_html = f'<span style="color:green">{value[0]}</span>'
+        elif value[0].lower() == "fail":
+            value_html = f'<span style="color:red">{value[0]}</span>'
         else:
-            value_html = value
+            value_html = value[0]
         comment += f"| re.sub(r'\w+', lambda m:m.group(0).capitalize(), {key}) | {value_html} |\n"
     
     comment += "</details>"
