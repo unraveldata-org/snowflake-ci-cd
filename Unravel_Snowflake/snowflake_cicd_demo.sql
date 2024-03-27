f395ae44856c23cd66718d17fb021ef885598d44
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-28 01:31:09 +0530

    Update snowflake_cicd_demo.sql

--- Unravel_Snowflake/snowflake_cicd_demo.sql
+++ Unravel_Snowflake/snowflake_cicd_demo.sql
@@ -9,3 +9,5 @@ SELECT *
 
 SELECT * FROM table13 INNER JOIN table4 order by table1.id limit 10;
 
+select WR_ORDER_NUMBER,WR_ITEM_SK from sfsalesshared_sfc_samples_va3_sample_data.tpcds_sf100tcl.web_returns where wr_fee >= 0.22 order by WR_ORDER_NUMBER;","SELECT tmp.query_id FROM UNRAVEL_SHARE10.SCHEMA_4823_T.query_history_temp tmp WHERE NOT EXISTS (SELECT query_id FROM QUERY_PROFILE WHERE query_id = tmp.query_id)
+
