cff246d107555c92f883b704977fc3b17d257c3e
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-28 02:26:50 +0530

    Update snowflake_cicd_demo.sql

--- Unravel_Snowflake/snowflake_cicd_demo.sql
+++ Unravel_Snowflake/snowflake_cicd_demo.sql
@@ -1,13 +1,13 @@
-SELECT * FROM employee631111212 CROSS JOIN orders21 order by employee84.id;
+SELECT * FROM employee6311111212 CROSS JOIN orders21 order by employee84.id;
 
 
 SELECT * 
- FROM employee69121131122
+ FROM employee691211311122
  CROSS JOIN orders2721 
  order by employee6912.id;
 
 
-SELECT * FROM table13111212 INNER JOIN table4 order by table1.id limit 10;
+SELECT * FROM table131111212 INNER JOIN table4 order by table1.id limit 10;
 
 
 
