9c0765efa90470e09a1bd9025d0764587cbbeeae
Suryansh Baranwal <152257717+sbaranwal10@users.noreply.github.com>
2024-03-28 13:42:43 +0530

    Update test187461984.sql

--- Unravel_Snowflake/test187461984.sql
+++ Unravel_Snowflake/test187461984.sql
@@ -1,16 +1,6 @@
 
 
-SELECT * FROM employee63111113212 CROSS JOIN orders21 order by employee84.id;
 
-
-SELECT * 
- FROM employee6912113113122
- CROSS JOIN orders2721 
- order by employee6912.id;
-
-
-SELECT * FROM table1311311212 INNER JOIN table4 order by table1.id limit 10;
-
-
+SELECT * FROM eorders CROSS JOIN orders21 order by employee84.id;
 
 
